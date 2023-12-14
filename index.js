fc2cc177f4d5ba8b4c34b8815c074f3ef5a3df71
Naym Hossen <naym@Nayms-MacBook-Pro.local>
2023-12-15 01:27:12 +0600

    create category route

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
-/// Middle Ware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://enOfficialServer:aT8HZNweMW3ohb6L@firstpractice.poejscf.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URL;
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,22 +28,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db("enOfficial").collection("users");
+    const userCollection = client.db("urbanHaven").collection("users");
+    const categoryNameCollection = client.db("urbanHaven").collection("categories");
 
+    /// category Section\\\\\\
+    app.get("/categories", async (req, res) => {
+      const result = await categoryNameCollection.find().toArray();
+      res.send(result);
+    });
 
-    //// User Collections 
-    app.post('/users', async (req, res) => {})
+    ////////// User Registration Section\\\\\\\\\
+    app.get("/registers", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/users', async (req, res) => {})
-    
-    app.get('/users/:id', async (req, res) => {})
-
-
-
+    app.post("/registers", async (req, res) => {
+      const newData = req.body;
+      const result = await userCollection.insertOne(newData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB! ✅ 🔥");
+    console.log("Pinged your deployment. You successfully connected to MongoDB! 🔥 🔥 🔥 ");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -49,9 +61,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World! ✅");
+  res.send("Hello World! 🔥");
 });
 
 app.listen(port, () => {
-  console.log(`EnOfficial app Running on port ${port} 🔥`);
+  console.log(`En server app listening on port ${port} 🔥`);
 });
